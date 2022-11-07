e7a8fb4f0e0992dd2990280ce959be207a324f60
fathergasc <quaglioniandrea@gmail.com>
2022-11-07 13:28:14 +0100

    add: migration cuisines/products/orders tables

---
+++ database/migrations/2022_11_07_120615_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('slug', 100)->unique();
+            $table->text('description')->nullable();
+            $table->unsignedDecimal('price', 8, 2);
+            $table->string('image', 255)->nullable();
+            $table->boolean('displayed')->default(1);
+            $table->string('category', 70)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
