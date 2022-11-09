91ada14aa33a2d594aabad17c381b83ca178f967
baly <kevin@balydev.it>
2022-11-09 14:41:23 +0100

    fixed: orders index query

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -16,28 +16,22 @@ class OrderController extends Controller
 
     public function index()
     {
-<<<<<<< HEAD
-        
-=======
-        /*
         //recover authenticated user id
         $id = Auth::id();
-        $user = Auth::user();
+        $user = User::find($id);
 
         //get restaurant of authenticated user
-        $userRestaurant = Restaurant::all()->where('user_id', $id)->first();
-        //dd($userRestaurant->id);
+        $restaurant = Restaurant::all()->where('user_id', $id)->first();
+        $products = Product::all()->where('restaurant_id', $restaurant->id);
 
-        //get all products from the restaurant of the authenticated user
-        $products = Product::all()->where('restaurant_id', $userRestaurant->id);
-
-        $userData = User::with('restaurants.products.orders')->where('id', $id)->get()->toArray();
-        dd($orders); */
-
-        $orders = Order::all();
+        $orders = [];
+        foreach($products as $product){
+            foreach($product->orders as $order){
+                $orders[] = $order;
+            }
+        }
 
         return view('admin.orders.index', compact('orders'));
->>>>>>> 690ea9db6af16c4c9fc109d903cb8a9b0abbfef3
     }
 
     public function create()
