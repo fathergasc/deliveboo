e1baf328e34bd5770f667d22a60509fcc09cde31
Marco Braccini <marco.braccini@live.it>
2022-11-10 12:52:13 -0600

    Add create product client validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script type="text/javascript" src="{{ asset('js/validation.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/front.js', 'public/js')
+    .js('resources/js/validation.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
