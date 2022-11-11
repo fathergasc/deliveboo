d4bcdbadd66329ff89256c787ec9481106f7e496
fathergasc <quaglioniandrea@gmail.com>
2022-11-11 12:42:17 +0100

    refactor: created Policies to protect all products routes and orders.show

---
+++ app/Policies/OrderPolicy.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Policies;
+
+use App\Order;
+use App\Product;
+use App\Restaurant;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class OrderPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Order  $order
+     * @return mixed
+     */
+    public function view(User $user, Order $order)
+    {
+        //if the function returns true, the user is authorized
+
+        $userRestaurant = Restaurant::all()->where('user_id', $user->id)->first();
+        $products = Product::all()->where('restaurant_id', $userRestaurant->id);
+
+        $orders = [];
+        //array containing only unique IDs of orders
+        $ordersIds = [];
+        foreach($products as $product){
+            foreach($product->orders as $order){
+                if(!in_array($order->id, $ordersIds)) {
+                    $ordersIds[] = $order->id;
+                    $orders[] = $order;
+                }
+            }
+        }
+
+        if(in_array($order->id, $ordersIds )) {
+            return true;
+        }
+
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Order  $order
+     * @return mixed
+     */
+    public function update(User $user, Order $order)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Order  $order
+     * @return mixed
+     */
+    public function delete(User $user, Order $order)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Order  $order
+     * @return mixed
+     */
+    public function restore(User $user, Order $order)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Order  $order
+     * @return mixed
+     */
+    public function forceDelete(User $user, Order $order)
+    {
+        //
+    }
+}
