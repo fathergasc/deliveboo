b99b3e4b19fadf6204843f581c32e13477af519a
Marco Braccini <marco.braccini@live.it>
2022-11-14 12:23:02 -0600

    cart work in progress

--- resources/js/pages/RestaurantMenuPage.vue
+++ resources/js/pages/RestaurantMenuPage.vue
@@ -1,10 +1,52 @@
 <template>
-    <div class="container">
-        <h2>Restaurant: {{restaurant.name}}</h2>
-        <h3>Menu:</h3>
-        <ul>
-            <li v-for="(product, index) in restaurant.products" :key="index">{{product.name}}</li>
-        </ul>
+    <div>
+
+        <section class="container-md text-center">
+            <div class="row">
+                <div class="col-4 offset-4">
+                    <img class="img-fluid" :src=" restaurant.image == null ? '/assets/img/food-main-logo_edit.png' : 'storage/'+ restaurant.image" :alt="restaurant.name">
+                </div>
+
+                <div class="col-4 offset-4">
+                    <h2>{{restaurant.name}}</h2>
+                </div>
+
+                <div class="col-12">
+                    <h3>Menu</h3>
+                    <ul class="list-group">
+                        <li class="list-group-item d-flex justify-content-between align-items-center text-capitalize"
+                        v-for="(product, index) in restaurant.products" :key="index">
+                            {{product.name}}
+                        <div>
+                            <div class="btn-group" role="group" aria-label="Basic example">
+                                <button type="button" class="btn btn-primary" @click="productDecrement(index)" :disabled="product.productCounter<=0">-</button>
+                                <div class="my_product-counter">{{liveProductCounter}}</div>
+                                <button type="button" class="btn btn-primary" @click="productIncrement(index)">+</button>
+                            </div>
+                            <button class="btn btn-primary" @click="addProductToCart(index)">Add</button>
+                        </div>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-12">
+                    <h3>Cart</h3>
+                    <ul class="list-group">
+                        <li class="list-group-item d-flex justify-content-between align-items-center text-capitalize"
+                        v-for="(product, index) in liveCart" :key="index">
+                            {{product.name}}
+                        <div class="d-flex">
+                            <div class="my_product-counter">{{product.productCounter}}</div>
+                            <button type="button" class="btn btn-primary" @click="productIncrement(index)">Del</button>
+                        </div>
+                        </li>
+                    </ul>
+                    <button class="btn btn-primary">Pay</button>
+                </div>
+            </div>
+        </section>
+
+
         <router-link :to="{name: 'home'}">Back</router-link>
     </div>
 
@@ -15,24 +57,51 @@ export default {
     name: 'RestaurantMenuPage',
     data() {
         return {
-            restaurant: null,
+            restaurant: [],
+            liveProductCounter: 0,
+            liveCart: []
         }
     },
     methods: {
-        getRestaurantSlug() {
+        getRestaurant() {
             const slug = this.$route.params.slug;
             axios.get('/api/restaurants/' + slug)
             .then((response)=>{
                 this.restaurant = response.data.results;
+
+                for (let i = 0; i < this.restaurant.products.length; i++) {
+                    this.restaurant.products[i].productCounter = 0;
+                }
+                console.log(this.restaurant.products[0].productCounter)
             })
+        },
+        productIncrement(index) {
+            this.restaurant.products[index].productCounter++;
+            //this.restaurant.products[index].productCounter = productCounter;
+            console.log(this.restaurant.products[index].productCounter)
+        },
+        productDecrement(index) {
+            this.restaurant.products[index].productCounter--;
+            console.log(this.restaurant.products[index].productCounter)
+        },
+        addProductToCart(index) {
+            this.liveCart.push(this.restaurant.products[index]);
+            console.log(this.liveCart)
         }
     },
     mounted() {
-        this.getRestaurantSlug();
+        this.getRestaurant();
+    },
+    computed: {
+        showProductCounter() {
+
+        }
     }
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+    .my_product-counter {
+        background-color: pink;
+    }
 </style>
