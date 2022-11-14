5261b41fa89cbc6307f524e577db40bf8671b282
Marco Braccini <marco.braccini@live.it>
2022-11-13 20:22:12 -0600

    webpack path update

--- public/js/front.js
+++ public/js/front.js
@@ -19224,7 +19224,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\quagl\Desktop\Boolean\deliveboo\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Users\marco\Desktop\Dev\Boolean\Project Work\deliveboo\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
