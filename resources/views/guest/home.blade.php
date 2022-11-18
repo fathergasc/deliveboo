968020ba75a9335f5616958c59dccbd264db1490
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-11-18 20:31:26 +0100

    add: favicon and title

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -4,8 +4,9 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Guest view</title>
+        <title>DeliveBoo</title>
         <link href="{{asset('css/app.css')}}" rel="stylesheet" />
+        <link rel="shortcut icon" href="/assets/img/super-french-fries_edit" type="image/x-icon">
     </head>
 
     <body>
