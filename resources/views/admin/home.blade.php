0a34c7069d09691c6ae9c2972644be1563d07d65
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-11-23 18:32:34 +0100

    fix: upload message dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,14 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-@if (session('status'))
+
+
+<div class="container">
+
+    @if (session('status'))
     <div class="alert alert-success" role="alert">
         {{ session('status') }}
     </div>
-@endif
+    @endif
 
-<div class="container">
-    <h1 class="text-center">Welcome to your dashboard, {{$user = Auth::user()->name}}</h1>.
+    <h1 class="text-center">Welcome to your dashboard, {{$user = Auth::user()->name}}</h1>
 </div>
 
 <div class="container">
